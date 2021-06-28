e40297e223a6f7f6c4177103828fd9c3bdb273c4
Bedrock02 <jimsteve91@gmail.com>
2021-06-27 22:26:24 -0400

    Add strava link to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,24 +4,29 @@ import { Icon, Grid } from 'semantic-ui-react';
 const Footer = () => (
   <Grid>
     <Grid.Row className="footer" textAlign="center" style={{padding: "50px 0"}}>
-      <Grid.Column textAlign="center" width={4}>
+      <Grid.Column textAlign="center" width={3}>
         <a href="https://github.com/Bedrock02/" target="_blank" rel="noopener noreferrer">
           <Icon link name='github' size='huge'/>
         </a>
       </Grid.Column>
-      <Grid.Column textAlign="center" width={4}>
+      <Grid.Column textAlign="center" width={3}>
+        <a href="https://www.linkedin.com/in/steven-jimenez-7a435251/" target="_blank" rel="noopener noreferrer">
+          <Icon link name='linkedin' size='huge' />
+        </a>
+      </Grid.Column>
+      <Grid.Column textAlign="center" width={3}>
         <a href="https://medium.com/@Bedrock02" target="_blank" rel="noopener noreferrer">
           <Icon link name='medium' size='huge' />
         </a>
       </Grid.Column>
-      <Grid.Column textAlign="center" width={4}>
+      <Grid.Column textAlign="center" width={3}>
         <a href="https://www.instagram.com/viens_voir02/" target="_blank" rel="noopener noreferrer">
           <Icon link name='instagram' size='huge' />
         </a>
       </Grid.Column>
-      <Grid.Column textAlign="center" width={4}>
-        <a href="https://www.linkedin.com/in/steven-jimenez-7a435251/" target="_blank" rel="noopener noreferrer">
-          <Icon link name='linkedin' size='huge' />
+      <Grid.Column textAlign="center" width={3}>
+        <a href="https://www.strava.com/athletes/6644207" target="_blank" rel="noopener noreferrer">
+          <Icon link name='strava' size='huge' />
         </a>
       </Grid.Column>
     </Grid.Row>

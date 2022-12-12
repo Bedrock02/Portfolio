f0e19d99ce13810f7f7937dbec68e35ee7e9a26f
Bedrock02 <jimsteve91@gmail.com>
2022-12-12 09:51:27 -0500

    [New] WorkItem Component & [New] Work Experience

--- src/assets/img/index.js
+++ src/assets/img/index.js
@@ -2,9 +2,11 @@ import nomad from './nomad_logo.png';
 import smImage from './sm_logo.png';
 import tunein from './tunein.jpg';
 import dnt from './dt_logo_robot.png';
+import even from './even_logo.png'
 export {
     nomad,
     smImage,
     tunein,
-    dnt
+    dnt,
+    even
 };
--- src/components/WorkExperience/index.js
+++ src/components/WorkExperience/index.js
@@ -5,41 +5,30 @@ import {
     smImage,
     tunein,
     dnt,
+    even,
 } from '../../assets/img'
+
+const WorkItem = ({ image, date }) => (
+    <>
+        <Grid.Column className='work-logos' stackable="true" textAlign="center" width={8}>
+            <Image centered src={image} size="medium" />
+        </Grid.Column>
+        <Grid.Column stackable="true" textAlign="center" width={8}>
+            <h2>{date}</h2>
+        </Grid.Column>
+    </>
+)
 
 const WorkExperience = () =>(
     <Grid.Row id="work" className="work-experience" centered verticalAlign="middle" style={{padding: "100px 0"}}>
         <Grid.Column stackable="true" textAlign="center" width={16}>
-        <h1>Work Experience</h1>
+            <h1>Work Experience</h1>
         </Grid.Column>
-
-        <Grid.Column className='work-logos' stackable="true" textAlign="center" width={8}>
-        <Image centered src={nomad} size="medium" />
-        </Grid.Column>
-        <Grid.Column stackable="true" textAlign="center" width={8}>
-        <h2>2019 - Present</h2>
-        </Grid.Column>
-
-        <Grid.Column className='work-logos' stackable="true" textAlign="center" width={8}>
-        <Image centered src={dnt} size="medium" />
-        </Grid.Column>
-        <Grid.Column stackable="true" textAlign="center" width={8}>
-        <h2>2018 - 2019</h2>
-        </Grid.Column>
-
-        <Grid.Column className='work-logos' stackable="true" textAlign="center" width={8}>
-        <Image centered src={smImage} size="medium" />
-        </Grid.Column>
-        <Grid.Column stackable="true" textAlign="center" width={8}>
-        <h2>2015-2018</h2>
-        </Grid.Column>
-
-        <Grid.Column className='work-logos' stackable="true" textAlign="center" width={8}>
-        <Image centered src={tunein} size="medium" />
-        </Grid.Column>
-        <Grid.Column stackable="true" textAlign="center" width={8}>
-        <h2>2014</h2>
-        </Grid.Column>
+        <WorkItem image={even} date={'2019 - 2022'}/>
+        <WorkItem image={nomad} date={'2019 - 2021'}/>
+        <WorkItem image={dnt} date={'2018 - 2019'}/>
+        <WorkItem image={smImage} date={'2015 - 2018'}/>
+        <WorkItem image={tunein} date={'2014'}/>
     </Grid.Row>   
 );
 export default WorkExperience;

86e4ca69649d5407e45ed2097f802f291804ceec
Bedrock02 <jimsteve91@gmail.com>
2022-12-19 10:38:24 -0500

    Added tools to work experience

--- src/components/WorkExperience/index.js
+++ src/components/WorkExperience/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Image, Grid } from 'semantic-ui-react';
-import { work, experience, grow } from './work.module.css'
+import { Image, Grid, List } from 'semantic-ui-react';
+import { work, experience, grow, toolContainer } from './work.module.css'
 import {
     nomad,
     smImage,
@@ -15,58 +15,116 @@ const workExperienceData = [
         href: 'https://evenfinancial.com/',
         date: '2021 - 2022',
         title: 'Software Engineer',
+        tools: [
+            'Nx.js',
+            'Typescript',
+            'RJSF',
+            'React',
+            'Cucumber',
+            'Docker',
+            'Unleash',
+        ]
     },
     {
         image: nomad,
         href: 'https://nomadhealth.com/',
         date: '2019 - 2021',
-        title: 'Technical Lead'
+        title: 'Technical Lead',
+        tools: [
+            'Next.js',
+            'React',
+            'MongoDB',
+            'Python',
+            'Flask',
+            'Celery',
+            'Jinja',
+            'MaterialUI'
+        ],
     },
     {
         image: dnt,
         href: 'https://domandtom.com/',
         date: '2018 - 2019',
-        title: 'Technical Lead'
+        title: 'Technical Lead',
+        tools: [
+            'React',
+            'GraphQL',
+            'Selenium',
+            'React Native',
+            'Ruby on Rails',
+            'Contentful',
+            'Bootstrap'
+        ]
     },
     {
         image: smImage,
         href: 'https://www.surveymonkey.com/',
         date: '2015 - 2018',
-        title: 'Software Engineer'
+        title: 'Software Engineer',
+        tools: [
+            'Backbone.js',
+            'JQuery',
+            'Python',
+            'Pyramid',
+            'Flask',
+            'SQL',
+            'Docker',
+            'AWS',
+            'CSS'
+        ]
     },
     {
         image: tunein,
         href: 'https://tunein.com/',
         date: '2014 - 2014',
-        title: 'Software Engineer'
+        title: 'Software Engineer',
+        tools: [
+            '.Net',
+            'Javascript',
+            'JQuery',
+        ]
     },
 ]
 
-const WorkItem = ({ image, href, date, title }) => (
+const WorkItem = ({ image, href, date, title, tools }) => (
     <Grid.Row className={work} centered verticalAlign="middle">
-        <Grid.Column className='work-logos' stackable="true" textAlign="center" width={8}>
+        <Grid.Column className='work-logos' textAlign="center" width={8}>
             <div className={grow}>
                 <a href={href} target='_blank'>
                     <Image centered src={image} size="medium" />
                 </a>
             </div>
         </Grid.Column>
-        <Grid.Column stackable="true" textAlign="center" width={8}>
-            <h2>{date}</h2>
-            <h2>{title}</h2>
+        <Grid.Column textAlign="center" width={8}>
+                <div>
+                    <h2>{date}</h2>
+                    <h2>{title}</h2>
+                </div>
+                <div className={toolContainer}>
+                    <List divided relaxed  horizontal>
+                        {tools.map( tool => (
+                            <List.Item>
+                                <List.Content>
+                                    {tool}
+                                </List.Content>
+                            </List.Item>
+                        ))}
+                    </List>
+                </div>
         </Grid.Column>
+            
     </Grid.Row>
 )
 
 const WorkExperience = () =>(
-    <Grid padded>
+    <Grid padded stackable>
         <Grid.Row id="work" className={experience} centered verticalAlign="middle" style={{ padding: "100px 0" }}>
-            <Grid.Column stackable="true" textAlign="center" width={16}>
+            <Grid.Column textAlign="center" width={16}>
                 <h1>Work Experience</h1>
             </Grid.Column>
         </Grid.Row>
-        {workExperienceData.map( ({image, href, date, title}) => (
-            <WorkItem image={image} href={href} date={date} title={title} />
+        {workExperienceData.map( (item) => (
+            <WorkItem {...item} />
         ))}
     </Grid>
 );

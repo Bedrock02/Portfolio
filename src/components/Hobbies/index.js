27a7d1ef3fd34da16a71e2b98f51a80ab45a9c1e
Bedrock02 <jimsteve91@gmail.com>
2022-12-19 12:51:19 -0500

    Hobbies section & clean up

--- src/components/Hobbies/index.js
+++ src/components/Hobbies/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
-import { Grid, Header } from 'semantic-ui-react';
+import { Grid, Image } from 'semantic-ui-react';
+import VideoLooper from 'react-video-looper';
+import cyclingVideo from '../../assets/videos/cycling.mp4'
+import timessquare from '../../assets/videos/timessquare.mp4'
+import botanical from '../../assets/videos/botanical-garden.mp4'
+import lucky from '../../assets/videos/lucky-trail.mp4'
 
 const Hobbies = () => (
   <>
-  <Grid.Row centered>
-    <Grid.Column textAlign="center">
-    <h1 header>Off The Keyboard</h1>
-    </Grid.Column>
-  </Grid.Row>
-  </>
+      <Grid.Row centered verticalAlign='middle'>
+        <Grid.Column textAlign="center" width={16} style={{ padding: "100px 0" }}>
+          <h1>Life Outside Of Work</h1>
+        </Grid.Column>
+      </Grid.Row>
+
+      <Grid.Row columns={2}>
+        <Grid.Column>
+            <VideoLooper source={cyclingVideo} start={0.00} end={5.00} autoplay height="50vh"/>
+        </Grid.Column>
+        <Grid.Column>
+            <VideoLooper source={botanical} start={0.00} end={10.00} autoplay height="50vh"/>
+        </Grid.Column>
+        <Grid.Column>
+            <VideoLooper source={timessquare} start={2.00} end={15.00} autoplay height="50vh"/>
+        </Grid.Column>
+        <Grid.Column>
+            <VideoLooper source={lucky} start={4.00} end={7.00} autoplay height="50vh"/>
+        </Grid.Column>
+      </Grid.Row>
+    </>
 )
+
+export default Hobbies;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,14 @@ import Layout from '../components/Layout/index.js';
 import Roles from '../components/Roles';
 import WorkExperience from '../components/WorkExperience'
 import Profile from '../components/Profile'
+import Hobbies from '../components/Hobbies/index.js';
 
 const IndexPage = () => (
   <Layout>
     <Profile />
-    <Roles isHorizontal={false}/>
+    <Roles />
     <WorkExperience />
+    <Hobbies />
   </Layout>
 );
 export default IndexPage;

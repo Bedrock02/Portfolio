0d17c0f42b50955ec9cccf6f97b1a905975df399
Bedrock02 <jimsteve91@gmail.com>
2022-12-21 12:12:16 -0500

    Adding meta tags

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -20,21 +20,38 @@ const Layout = ({ children, data }) => (
         }
       }
     `}
+        
     render={data => (
+      
       <>
         <Helmet
           title={data.site.siteMetadata.title}
           meta={[
-            {
-              name: 'description',
-              content: 'Steven Jimenez is a web developer, community leader, and a public speaker' },
             { name: 'keywords', content: 'Steven Jimenez, portfolio, Bedrock02' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1'}
           ]}
           link={[
             { rel: 'shortcut icon', type: 'image/png', href: `${favicon}` }
           ]}
-        />
+        >
+          <title>Steven Jimenez</title>
+          <meta name="description" content="Steven Jimenez is a web developer, community leader, and a public speaker" />
+
+          {/* Facebook Meta Tags */}
+          <meta property="og:url" content="https://wepadev.com/" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Steven Jimenez" />
+          <meta property="og:description" content="Steven Jimenez is a web developer, community leader, and a public speaker" />
+          <meta property="og:image" content="https://wepadev.com/static/profile2-3436f6fd1dbdc38c13e9ec70ef939e22.jpg" />
+
+          {/* Twitter Meta Tags */}
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta property="twitter:domain" content="wepadev.com" />
+          <meta property="twitter:url" content="https://wepadev.com/" />
+          <meta name="twitter:title" content="Steven Jimenez" />
+          <meta name="twitter:description" content="Steven Jimenez is a web developer, community leader, and a public speaker" />
+          <meta name="twitter:image" content="https://wepadev.com/static/profile2-3436f6fd1dbdc38c13e9ec70ef939e22.jpg" />
+        </Helmet>
         <div style={{
           background: `url("${image}")`,
         }}>

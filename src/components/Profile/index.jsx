6a75e8515fddedd7178cf2867fec336a98152be1
Bedrock02 <jimsteve91@gmail.com>
2023-01-07 11:50:15 -0500

    Roles clean up

---
+++ src/components/Profile/index.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Image, Grid, Header } from 'semantic-ui-react';
+import { fadeIn, fullName } from './profile.module.css';
+import profilePicture from '../../assets/img/profile2.jpg';
+import AnimatedButton from '../AnimatedButton';
+
+function Profile() {
+  return (
+    <>
+      <Grid.Row style={{ marginTop: '150px' }} centered>
+        <Grid.Column textAlign="center">
+          <Image style={{ marginTop: '20px' }} centered rounded src={profilePicture} size="medium" />
+        </Grid.Column>
+      </Grid.Row>
+
+      <Grid.Row centered>
+        <Grid.Column textAlign="center">
+          <div className={fadeIn}>
+            <Header as="h1">
+              {'Hey, I\'m'}
+              <span className={fullName}>Steven Jimenez.</span>
+            </Header>
+            <Header.Subheader as="h3">&quote;Problems I gravitate towards are areas of pain points and weakness.&quote;</Header.Subheader>
+            <AnimatedButton
+              primary
+              title="Resume"
+              iconName="file pdf outline"
+              onClick={() => {
+                window.open('https://bedrock02.github.io/resume/', '_blank');
+              }}
+            />
+          </div>
+        </Grid.Column>
+      </Grid.Row>
+    </>
+  );
+}
+
+export default Profile;
---
+++ src/components/Roles/index.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import { Grid, Icon, List } from 'semantic-ui-react';
+import {
+  group, dev, speaking, leader, link,
+} from './roles.module.css';
+
+const toolNames = ['html5', 'sass', 'js', 'react', 'python', 'git', 'docker', 'aws'];
+const leaderExamples = [
+  {
+    title: 'Mock Interviews With Pursuit',
+    href: 'https://www.pursuit.org/volunteer',
+  },
+  {
+    title: 'Fundraising For The Future Of Tech',
+    href: 'https://medium.com/@Bedrock02/fundraising-for-the-future-of-tech-2353768d813a',
+  },
+  {
+    title: 'Content Creation for The Longhairs',
+    href: 'https://blog.thelonghairs.us/beyond-the-locks-alejandro-carillo/',
+
+  },
+];
+
+function Roles() {
+  return (
+    <Grid.Row id="about" columns={3} className={group}>
+      <Grid.Column textAlign="center" className={dev}>
+        <Icon name="computer" size="huge" />
+        <h2>Software Engineer</h2>
+        <p>
+          My passion is creating web applications with the latest and greatest tools.
+          My design approach is simple, clean and crisp.
+        </p>
+        <h3>Tool Kit</h3>
+        <List horizontal>
+          {toolNames.map((name) => (
+            <List.Item><Icon name={name} size="big" /></List.Item>
+          ))}
+        </List>
+      </Grid.Column>
+
+      <Grid.Column textAlign="center" className={speaking}>
+        <Icon name="microphone" size="huge" style={{ color: 'white' }} />
+        <h2>Public Speaker</h2>
+        <p>
+          Our stories should be shared with the future generation.
+          My story is unique and relatable to many.
+        </p>
+        <h3>Audiences</h3>
+        <List>
+          <List.Item>Non Profit Organizations</List.Item>
+          <List.Item>High School/College</List.Item>
+          <List.Item>Tech Communities</List.Item>
+        </List>
+      </Grid.Column>
+
+      <Grid.Column textAlign="center" className={leader}>
+        <Icon name="handshake outline" size="huge" color="blue" />
+        <h2>Community Leader</h2>
+        <p>
+          In order to see the change we want to see,
+          we as individuals need to invest in our communities.
+        </p>
+        <h3>Contributions</h3>
+        <List>
+          {leaderExamples.map(({ title, href }) => (
+            <List.Item>
+              <a className={link} target="_blank" rel="noopener noreferrer" href={href}>
+                {title}
+              </a>
+            </List.Item>
+          ))}
+        </List>
+      </Grid.Column>
+    </Grid.Row>
+  );
+}
+
+export default Roles;

d57cd9ed606e3818d4095fa30bd6fb711189f023
Bedrock02 <jimsteve91@gmail.com>
2023-01-07 12:10:58 -0500

    Work Experience Clean up

---
+++ src/components/WorkExperience/index.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { Image, Grid, List } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import {
+  work, experience, grow, toolContainer,
+} from './work.module.css';
+import { workExperienceData } from './data';
+
+function WorkItem(item) {
+  const {
+    image, href, date, title, tools,
+  } = item;
+  return (
+    <Grid.Row className={work} centered verticalAlign="middle">
+      <Grid.Column className="work-logos" textAlign="center" width={8}>
+        <div className={grow}>
+          <a href={href} target="_blank" rel="noreferrer">
+            <Image centered src={image} size="medium" />
+          </a>
+        </div>
+      </Grid.Column>
+      <Grid.Column textAlign="center" width={8}>
+        <div>
+          <h2>{date}</h2>
+          <h2>{title}</h2>
+        </div>
+        <div className={toolContainer}>
+          <List divided relaxed horizontal>
+            {tools.map((tool) => (
+              <List.Item>
+                <List.Content>
+                  {tool}
+                </List.Content>
+              </List.Item>
+            ))}
+          </List>
+        </div>
+      </Grid.Column>
+    </Grid.Row>
+  );
+}
+
+WorkItem.propTypes = {
+  item: PropTypes.shape({
+    image: PropTypes.string,
+    href: PropTypes.string,
+    date: PropTypes.string,
+    title: PropTypes.string,
+    tools: PropTypes.string,
+  }),
+};
+
+WorkItem.defaultProps = {
+  item: PropTypes.shape({
+    image: '',
+    href: '#',
+    date: '',
+    title: 'title',
+    tools: [],
+  }),
+};
+
+function WorkExperience() {
+  return (
+    <Grid padded stackable>
+      <Grid.Row id="work" className={experience} centered verticalAlign="middle" style={{ padding: '100px 0' }}>
+        <Grid.Column textAlign="center" width={16}>
+          <h1>Work Experience</h1>
+        </Grid.Column>
+      </Grid.Row>
+      {workExperienceData.map((item) => (
+        <WorkItem item={item} />
+      ))}
+    </Grid>
+  );
+}
+export default WorkExperience;

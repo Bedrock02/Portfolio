9f343525cda43ec1bd65a0834f3c22b870e32cc5
Bedrock02 <jimsteve91@gmail.com>
2023-01-07 11:52:06 -0500

    dedicated data file for Roles component

---
+++ src/components/Roles/data.js
@@ -0,0 +1,16 @@
+export const toolNames = ['html5', 'sass', 'js', 'react', 'python', 'git', 'docker', 'aws'];
+export const leaderExamples = [
+  {
+    title: 'Mock Interviews With Pursuit',
+    href: 'https://www.pursuit.org/volunteer',
+  },
+  {
+    title: 'Fundraising For The Future Of Tech',
+    href: 'https://medium.com/@Bedrock02/fundraising-for-the-future-of-tech-2353768d813a',
+  },
+  {
+    title: 'Content Creation for The Longhairs',
+    href: 'https://blog.thelonghairs.us/beyond-the-locks-alejandro-carillo/',
+
+  },
+];
--- src/components/Roles/index.jsx
+++ src/components/Roles/index.jsx
@@ -4,22 +4,7 @@ import {
   group, dev, speaking, leader, link,
 } from './roles.module.css';
 
-const toolNames = ['html5', 'sass', 'js', 'react', 'python', 'git', 'docker', 'aws'];
-const leaderExamples = [
-  {
-    title: 'Mock Interviews With Pursuit',
-    href: 'https://www.pursuit.org/volunteer',
-  },
-  {
-    title: 'Fundraising For The Future Of Tech',
-    href: 'https://medium.com/@Bedrock02/fundraising-for-the-future-of-tech-2353768d813a',
-  },
-  {
-    title: 'Content Creation for The Longhairs',
-    href: 'https://blog.thelonghairs.us/beyond-the-locks-alejandro-carillo/',
-
-  },
-];
+import { toolNames, leaderExamples } from './data';
 
 function Roles() {
   return (
